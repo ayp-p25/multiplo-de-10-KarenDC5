50dd604a9482802af9d50a14ac4f84d95028d253
KarenDC5 <karen.duran@iteso.mx>
2025-02-17 15:53:43 -0600

    primer intento

--- multiplo_de_10.py
+++ multiplo_de_10.py
@@ -1,15 +1,23 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Determinar si un número dado es múltiplo de 10
+Karen Durán
+757136
+Ingeniería Civil
+Algoritmos y Programación 
+ESI232B2
+17/02/25
+minutos 
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero = int(input("Introduzca un número: "))
 
 # Proceso
-
+if numero % 10 == 0:
+    multiplo = 'si es múltiplo de 10' 
+else:
+    multiplo = 'no es múltiplo de 10' 
 
 # Salidas
-print(salida)
+print("El número", numero, multiplo)
+
